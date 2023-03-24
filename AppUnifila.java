c74bf1541f0c3880cf869ca426125036cbba726e
ricardo <ricardo.vargas.haro@uabc.edu.mx>
2023-03-24 00:03:27 -0700

    se crea la clase para la version unifila

---
+++ AppUnifila.java
@@ -0,0 +1,14 @@
+/**
+ * Esta clase representa una tienda de supermercado,
+ * pero con una fila única para todos los clientes.
+ * 
+ * Cada puede tener hasta 2 clientes. Hasta que se
+ * atienda a un cliente, no se puede formar uno nuevo.
+ * 
+ * La fila única se encuentra antes de las cajas, ahi
+ * se forman todos los clientes a esperar a que se
+ * atiendan los que ya están en las cajas.
+ */
+public class AppUnifila {
+    
+}
